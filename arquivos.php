f7fdc166cf72e16353da3ec6bfec747dc57763ab
Fernando Fagonde <fernando@ipsillon.cc>
2022-01-28 08:46:13 -0300

    revista espírita

---
+++ arquivos.php
@@ -0,0 +1,333 @@
+<!-- Portfolio Section -->
+<div id="works-section">
+    <div class="container">
+        <!-- Container -->
+        <div class="section-title text-center center">
+            <h2>Arquivos e Downloads</h2>
+            <hr>
+            <div class="clearfix"></div>
+            <p>Encontre aqui os arquivos disponíveis para download, vídeos e obras em pdf. Clique no botão abaixo para filtrar</p>
+        </div>
+        <div class="categories">
+            <ul class="cat">
+                <li>
+                    <ol class="type">
+                        <li><a href="#" data-filter="*" class="active">Todos</a></li>
+                        <li><a href="#" data-filter=".pentateuco">Obras Básicas</a></li>
+                        <li><a href="#" data-filter=".esde">ESDE</a></li>
+                        <li><a href="#" data-filter=".eade">EADE</a></li>
+                        <li><a href="#" data-filter=".revista">Revista Espírita</a></li>
+                    </ol>
+                </li>
+            </ul>
+            <div class="clearfix"></div>
+        </div>
+        <div class="row">
+            <div class="portfolio-items">
+                <div class="col-sm-6 col-md-3 col-lg-3 pentateuco">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/le.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>O Livro dos Espíritos</h4>
+                                    <small>Princípios da Doutrina Espírita.</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/le.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 pentateuco">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/ese.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Evangelho Segundo o Espiritismo</h4>
+                                    <small>A explicação das máximas morais do Cristo em concordância com o Espiritismo e suas aplicações às diversas circunstâncias da vida.</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/le.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 pentateuco">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/lm.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>O Livro dos Médiuns</h4>
+                                    <small>Guia dos médiuns e dos evocadores</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/le.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 pentateuco">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/ci.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>O céu e o inferno</h4>
+                                    <small>A justiça divina segundo o Espiritismo.</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/le.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-3 col-lg-3 pentateuco">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/ag.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>A Gênese</h4>
+                                    <small>Os milagres e as predições segundo o Espiritismo</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/le.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 esde">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/esde1.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>ESDE I</h4>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/esde1.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 esde">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/esde2.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>ESDE II</h4>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/esde1.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 esde">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/esdeu.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>ESDE Único</h4>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/esde1.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-3 col-lg-3 eade">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/eade1.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Livro I</h4>
+                                    <small>Cristianismo e Espiritismo</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/eade.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 eade">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/eade2.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Livro II</h4>
+                                    <small>Ensinos e Parábolas de Jesus - Parte 1</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/eade.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 eade">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/eade3.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Livro III</h4>
+                                    <small>Ensinos e Parábolas de Jesus - Parte 2</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/eade.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 eade">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/eade4.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Livro IV</h4>
+                                    <small>O Consolador prometido por Jesus</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/eade.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-3 col-lg-3 eade">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/eade5.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Livro V</h4>
+                                    <small>Filosofia e Ciência Espíritas</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/eade.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 revista">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/Revista1858.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Revista Espírita</h4>
+                                    <small>1858</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/re.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 revista">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/Revista1859.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Revista Espírita</h4>
+                                    <small>1859</small>
+
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/re.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 revista">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/Revista1860.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Revista Espírita</h4>
+                                    <small>1860</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/re.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-3 col-lg-3 revista">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/Revista1861.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Revista Espírita</h4>
+                                    <small>1861</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/re.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 revista">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/Revista1862.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Revista Espírita</h4>
+                                    <small>1862</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/re.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 revista">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/Revista1863.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Revista Espírita</h4>
+                                    <small>1863</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/re.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 revista">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/Revista1864.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Revista Espírita</h4>
+                                    <small>1864</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/re.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+
+                <div class="col-sm-6 col-md-3 col-lg-3 revista">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/Revista1865.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Revista Espírita</h4>
+                                    <small>1865</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/re.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 revista">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/Revista1866.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Revista Espírita</h4>
+                                    <small>1866</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/re.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 revista">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/Revista1867.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Revista Espírita</h4>
+                                    <small>1867</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/re.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 revista">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/Revista1868.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Revista Espírita</h4>
+                                    <small>1868</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/re.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3 col-lg-3 revista">
+                    <div class="portfolio-item">
+                        <div class="hover-bg"> <a href="arquivos/Revista1869.pdf" target="_blank">
+                                <div class="hover-text">
+                                    <h4>Revista Espírita</h4>
+                                    <small>1869</small>
+                                    <div class="clearfix"></div>
+                                </div>
+                                <img src="img/downloads/re.png" class="img-responsive" alt="Project Title">
+                            </a> </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
